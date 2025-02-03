5c3939721343ef093b49205070c2bdd2408f108e
Freddy Paguay <freddypaguay20@gmail.com>
2025-02-03 09:06:38 -0500

    create class

--- src/pages/create-class/CreateClassPage.tsx
+++ src/pages/create-class/CreateClassPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useAuth } from "../../context/AuthContext";
+import moment from 'moment';
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import {
